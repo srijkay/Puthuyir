8783b5a5f2a91846a180197fd3a110efe0748491
srijkay <srijkay@yahoo.com>
2018-09-28 12:14:17 -0400

    QA ready

--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import {SchoolRegistrationComponent} from './school-registration/school-registration.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
-import {SchoolListComponent} from './school-list/school-list.component'
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {SchoolListComponent} from './school-list/school-list.component';
+import {ViewShoolRegistrationComponent} from './view-shool-registration/view-shool-registration.component';
 const routes: Routes = [
   {
     path: 'schoolregistration',
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'schoollist',
     component: SchoolListComponent
+  },
+  {
+    path: 'viewSchoolReg',
+    component: ViewShoolRegistrationComponent
   },
   {
     path: '**',
--- angular-ui/src/app/school.service.ts
+++ angular-ui/src/app/school.service.ts
@@ -1,55 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Http,Headers } from '@angular/http';
-import {FormGroup} from '@angular/forms';
+import { Http, Headers } from '@angular/http';
+import { FormGroup } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { environment } from '../environments/environment';
 import { catchError, map, tap } from 'rxjs/operators';
 import { School } from './model/school';
-
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
 
+  schoolRegForm:FormGroup;
+  private schoolRegFormBS = new BehaviorSubject(this.schoolRegForm);
+  currentSchoolRegForm = this.schoolRegFormBS.asObservable();
+
+  url:string;
+  private imageUrlBS = new BehaviorSubject(this.url);
+  currentImageUrlBS = this.imageUrlBS.asObservable();
+
   constructor(private http: Http) { }
 
-  
-  /** POST: add a new hero to the server */
- /* registerSchool (school: School): Observable<School> {
-    return this._httpClient.post<School>('http://localhost:3000/schools', school, httpOptions).pipe(
-    catchError(this.handleError<School>('registerSchool'))
-  );
-}*/
+  enterSchoolRegister(schoolRegForm: FormGroup) {
+    this.schoolRegFormBS.next(schoolRegForm);
+  }
 
-registerSchool (school: any) {
-  return this.http.post(environment["school.register.url"], school);
-}
+  enterImageUrl(url: string) {
+    this.imageUrlBS.next(url);
+  }
 
-getSchoolList () {
-  return this.http.get(environment["school.register.url"]);
-}
+  registerSchool(school: any) {
+    return this.http.post(environment["school.register.url"], school);
+  }
 
-/**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T> (operation = 'operation', result?: T) {
+  getSchoolList() {
+    return this.http.get(environment["school.register.url"]);
+  }
+
+  /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-     console.log("Error happening for post call");
+      console.log("Error happening for post call");
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
- 
+
       // TODO: better job of transforming error for user consumption
       //this.log(`${operation} failed: ${error.message}`);
- 
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
