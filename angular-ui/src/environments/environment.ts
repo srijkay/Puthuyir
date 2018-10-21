389581556dba3a6e051ff061b6f4434f16ba8cae
srijkay <36108000+srijkay@users.noreply.github.com>
2018-10-21 14:24:04 -0400

    Add files via upload

---
+++ angular-ui/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  'state.lookup.url':'',
+  'district.lookup.url':'',
+  'city.lookup.url':'',
+  'reqtype.lookup.url':'',
+  'assettype.lookup.url':'',
+  'assetname.lookup.url':'',
+  'schooltype.lookup.url':'',
+  'school.register.url':'',
+  'school.list.url':'',
+  'school.image.dir':'',
+  'image.server.url':''
+  
+};
