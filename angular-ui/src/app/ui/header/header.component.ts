e74217dcb8036987653a63e559743dd2d3c618fc
srijkay <36108000+srijkay@users.noreply.github.com>
2018-10-31 09:41:52 -0400

    Update header.component.ts

--- angular-ui/src/app/ui/header/header.component.ts
+++ angular-ui/src/app/ui/header/header.component.ts
@@ -2,13 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-<<<<<<< HEAD
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
-=======
-  templateUrl: './header.component.html'
-  //styleUrls: ['./header.component.css']
->>>>>>> feature_Kamalkanth
+
 })
 export class HeaderComponent implements OnInit {
 
