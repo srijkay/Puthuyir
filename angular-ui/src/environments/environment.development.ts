fbf97722a7a6d84f734866121ded67c13997e2f6
srijkay <36108000+srijkay@users.noreply.github.com>
2018-10-31 09:36:40 -0400

    Update environment.development.ts

--- angular-ui/src/environments/environment.development.ts
+++ angular-ui/src/environments/environment.development.ts
@@ -1,18 +1,5 @@
 export const environment = {
   'production': false,
-<<<<<<< HEAD
-  'state.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/state',
-  'district.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/district?parentfield=state&parentkey=TN',
-  'city.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/city',
-  'reqtype.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/reqtype',
-  'assettype.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/assettype',
-  'assetname.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/asset',
-  'schooltype.lookup.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/lookup/field/schooltype',
-  'school.register.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/school',
-  'school.list.url':'http://localhost:8080/revamp-service-1.0-SNAPSHOT/school',
-  'school.image.dir':'http://localhost:8080/',
-  'image.server.url':'http://localhost:8080/images/'
-=======
   'state.lookup.url':'http://localhost:6060/lookup/field/state',
   'district.lookup.url':'http://localhost:6060/lookup/field/district?parentfield=state&parentkey=TN',
   'city.lookup.url':'http://localhost:6060/lookup/field/city',
@@ -24,5 +11,4 @@ export const environment = {
   'school.list.url':'http://localhost:6060/school',
   'school.image.dir':'http://localhost:6060/',
   'image.server.url':'http://localhost:6060/images/'
->>>>>>> feature_Kamalkanth
 };
